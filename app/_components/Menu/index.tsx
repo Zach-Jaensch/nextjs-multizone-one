cf1ca23fa3fbbb0ad979ca02f75efdfd77b53b69
Zach Jaensch <zach.jaensch@safetyculture.io>
2024-05-06 10:19:09 +1000

    use styled components. Move menu to layout

---
+++ app/_components/Menu/index.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+const UL = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  gap: 1rem;
+`;
+
+export function Menu() {
+  return (
+    <nav>
+      <UL>
+        <li>
+          <Link href="/">Home page for app 1</Link>
+        </li>
+        <li>
+          <Link href="/internal">Internal page for app 1</Link>
+        </li>
+        <li>
+          <a href="/other">Home page for app 2</a>
+        </li>
+        <li>
+          <a href="/other/internal">Internal page for app 2</a>
+        </li>
+      </UL>
+    </nav>
+  );
+}
--- app/internal/page.tsx
+++ app/internal/page.tsx
@@ -1,13 +1,7 @@
-import Link from "next/link";
-
 export default function Page() {
   return (
     <main>
       <h1>Internal page for app 1</h1>
-      <Link href="/">Home page for app 1</Link>
-      <Link href="/internal">Internal page for app 1</Link>
-      <a href="/other">Home page for app 2</a>
-      <a href="/other/internal">Internal page for app 2</a>
     </main>
   );
 }
